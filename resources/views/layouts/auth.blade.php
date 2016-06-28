665d6318ac1af8942a535455e9f8e0dc3768c0f1
Drew Beaman <dbeaman@sanbeaman-mbpr.local>
2016-06-28 13:00:56 -0400

    login tweek

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>@yield('title')AdminLTE EMU</title>
+	<title>@yield('title')Admin EMU</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	@section('style-vendor')
